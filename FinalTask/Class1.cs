5749a669f41844fab5841e92e91b2a7f5eb44aaa
Jetkarius <boroksid@yandex.ru>
2023-01-16 22:52:20 +0300

    Добавьте файлы проекта.

---
+++ FinalTask/Class1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace FinalTask
+{
+    [Serializable]
+    public class Student
+    {
+        public Student(string name, string group, DateTime dateOfBirth)
+        {
+            Name = name;
+            Group = group;
+            DateOfBirth = dateOfBirth;
+        }
+
+        public string Name { get; set; }
+        public string Group { get; set; }
+        public DateTime DateOfBirth { get; set; }
+
+        /// <summary>
+        /// Переопределенный метод для удобного отображения данных студента
+        /// </summary>
+        /// <returns>Строка, содержащая имя и дату рождения студента</returns>
+        public override string ToString()
+        {
+            return $"Имя: {Name}. Дата рождения: {DateOfBirth.ToShortDateString()}";
+        }
+    }
+}
---
+++ Module_8_Task_4/Program.cs
@@ -0,0 +1,46 @@
+﻿using FinalTask;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace Module_8_Task_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var workDir = @"C:\Students\";
+            var filePath = $"{workDir}Students.dat";
+
+            var formatter = new BinaryFormatter();
+            Student[] students;
+            using (var fs = new FileStream(filePath, FileMode.Open))
+            {
+                students = (Student[])formatter.Deserialize(fs);
+            }
+            Console.WriteLine($"Всего получено {students.Length} студентов");
+
+            var grouppedStudents = students.GroupBy(s => s.Group);
+
+            Console.WriteLine($"Всего найдено {grouppedStudents.Count()} групп");
+
+            foreach (var group in grouppedStudents)
+            {
+                var groupName = group.Key; 
+                var newFilePath = $"{workDir}{groupName}.txt";
+                List<string> newFileLines = new List<string>();
+                foreach (var student in group)
+                {
+                    var newLine = student.ToString(); 
+                    newFileLines.Add(newLine); 
+                }
+
+                File.AppendAllLines(newFilePath, newFileLines); 
+                Console.WriteLine($"Студенты группы {group.Key} записаны в файл {newFilePath}");
+            }
+        }
+    }
+}
+
